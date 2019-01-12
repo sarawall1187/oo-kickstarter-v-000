03d74a60465b8d0434a6533b77b9140ad947e9bd
Sara Walker <sarawall1187@gmail.com>
2019-01-12 17:39:41 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,15 @@
+require "pry"
 class Backer
-  attr_accessor :name, :backed_projects, :project
+  attr_accessor :name, :backed_projects
   
   def initialize(name)
     @name = name 
     @backed_projects = []
   end
+  
   def back_project(project)
     @backed_projects << project
-    # @backers.each {|new_backer| Project.add_backer(new_backer)}
+    project.add_backer(self)  
   end
+  
 end
