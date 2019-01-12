6dd90a967165118fcf7a78ae40d2f9fab23bf969
Sara Walker <sarawall1187@gmail.com>
2019-01-12 17:41:43 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,11 +8,7 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-<<<<<<< HEAD
     backer.backed_projects(self)
-=======
-    backer.back_project(self)
->>>>>>> af5ca4851463ab44520da1add5dc5dcad9fd7417
   end
   
   
